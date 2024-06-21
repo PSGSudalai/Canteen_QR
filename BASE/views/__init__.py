5ec7c81c74d0dcc6f6501fc1bcf1b50debdc343b
Vishva-01 <vishva01mail@gmail.com>
2024-06-21 14:08:01 +0530

    transaction revert and qr scan html page created

--- BASE/views/__init__.py
+++ BASE/views/__init__.py
@@ -1,4 +1,4 @@
 from .user import login_view, signup_view, logout_view, qr_image_view
 from .item import canteen_item_create, canteen_item_list
 from .add_cart import add_to_cart, CartListView, clear_cart_items, delete_cart_item
-from .transactions import recharge_transaction_view, payment_transaction_view
+from .qr_scan import qr_scan_view
