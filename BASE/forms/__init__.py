9af7f98e7662603c999bbcadd7cc6fee4f29233b
Vishva-01 <vishva01mail@gmail.com>
2024-06-25 22:59:49 +0530

    commit

--- BASE/forms/__init__.py
+++
@@ -1,2 +0,0 @@
-from .item import CanteenItemForm
-from .add_to_cart_fom import AddToCartForm
--- BASE/forms/add_to_cart_fom.py
+++
@@ -1,9 +0,0 @@
-# BASE/forms.py
-
-from django import forms
-from BASE.models import CanteenItems, Cart
-
-
-class AddToCartForm(forms.Form):
-    item = forms.ModelChoiceField(queryset=CanteenItems.objects.all())
-    quantity = forms.IntegerField(min_value=1, initial=1)
